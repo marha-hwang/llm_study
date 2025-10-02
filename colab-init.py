a92ac270aba3fcd85d76c24adee7292f6cb133f9
haram <marha-hwang>
2025-10-02 08:51:39 +0000

    llm-study init

--- colab-init.py
+++ colab-init.py
@@ -1,1 +1,7 @@
-print("hello")
+
+import torch
+print(f"CUDA 사용 가능: {torch.cuda.is_available()}")
+print(f"GPU 개수: {torch.cuda.device_count()}")
+if torch.cuda.is_available():
+    print(f"GPU 이름: {torch.cuda.get_device_name(0)}")
+    print(f"CUDA 버전: {torch.version.cuda}")
---
+++ transformer/token.py
@@ -0,0 +1,4 @@
+
+input_text = "나는 최근 파리를 다녀왔다"
+input_text_list = input_text.split()
+print(f"input_text_list : {input_text_list}")
